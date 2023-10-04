6520c97f8c9ae3ce778d4d296a515fa55b4f73c4
chadeqmartin <chadeqmartin@gmail.com>
2023-10-04 17:32:36 -0400

    updated with solution to challenge

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,23 @@
 function linearSearch(searchTerm, arr) {
+  
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === searchTerm){
+      return i;
+    }
+  }
   return undefined;
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+
+  let indexOfSearchMatches = [];
+
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] === searchTerm){
+      indexOfSearchMatches.push(i);
+    }
+  }
+  return indexOfSearchMatches;
 }
 
 module.exports = { linearSearch, globalLinearSearch };
